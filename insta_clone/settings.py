f7a716010dfd626c20d4e4e7ef6dda6b1de2d3cf
Amit Kumar Singh <kumarsingha922@gmail.com>
2021-05-08 14:34:53 +0530

    Update settings.py

--- insta_clone/settings.py
+++ insta_clone/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 
-# STATIC_ROOT = os.path.join('staticfiles')
+STATIC_ROOT = os.path.join('staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
